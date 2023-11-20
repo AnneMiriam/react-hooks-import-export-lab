950f823c81e9a2f66054a20cd396fcffec2d21b3
Anne Hastings <anne.miriam@gmail.com>
2023-11-19 21:11:32 -0700

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { image } from "/Users/annehastings/Development/Code/phase-2/react-hooks-import-export-lab/src/data/user.js"
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src='./images/demo.png' alt="I made this" />
+      <img src={image} alt="I made this" />
     </div>
   );
 }
